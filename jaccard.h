4094062d7e88722130d4a9c4922c70228f55517a
adriacabeza <adriacabeza@hotmail.com>
2018-12-07 22:51:14 +0100

    h file

---
+++ jaccard.h
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+#include <set>
+using namespace std;
+
+double intersection(const set<string>& A, const set<string>& B);
+
+double Jaccard(const set<string>& A, const set<string>& B);
