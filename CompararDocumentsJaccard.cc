c869821686c96c9d3ce68189b4a889355636645a
zemone <antoni.rambla.moiset@gmail.com>
2018-12-07 23:07:33 +0100

    changes

--- CompararDocumentsJaccard.cc
+++ CompararDocumentsJaccard.cc
@@ -1,6 +1,7 @@
 #include "kshingles.h"
 #include "jaccard.h"
 
+ifstream inFile;
 
 using namespace std;
 
@@ -10,25 +11,28 @@ int main(){
     int k;
     string path;
     cout<<"Indica el path del primer document "<<num<< " a comparar:"<<endl;
-    cin>> path; 
-    string path2; 
+    cin>> path;
+
+    inFile.open(path);
+    set<string> A = kshingles(inFile&,k,true,false,false);
+    inFile.close();
+
     cout<<"Indica el path del segon document "<<num<< " a comparar:"<<endl;
-    cin>> path2;
+    cin>> path;
+
+    inFile.open(path);
+    set<string> A = kshingles(inFile&,k,true,false,false);
+    inFile.close();
+
     cout<<"De quin tamany vols els shingles?"<<endl;
-    cin >> k; 
+    cin >> k;
     //TODO: ficar una opció que sigui amb words o amb lletres
     /*
-    bool decom; 
+    bool decom;
     cout<<"Vols descompondre el document en paraules(0) o caràcters(1)?"
     cin>>descom;
     */
     //ARA JA HO TINC TOT INICIALITZAT
-
-    ifstream ifs (path);
-    ifstream ifs2 (path2);
-
-    set<string> A = kshingles(ifs,k,true,false,false);
-    set<string> B = kshingles(ifs2,k,true,false,false);
     cout<<"La diferència calculada per la Jaccard Similarity és de: "<< Jaccard(A,B);
 
     cout<<"Temps d'execució final:"<<double(clock()-start)/CLOCKS_PER_SEC <<endl;
