a20c7fb498cf5e4f3524d33277aaf17216e08448
adriacabeza <adriacabeza@hotmail.com>
2018-12-09 02:49:02 +0100

    works

--- jaccardaprox.cc
+++ jaccardaprox.cc
@@ -5,11 +5,24 @@
 #include <vector>
 #include <set>
 #include <cmath>
+#include <map>
 #include "kshingles.h"
 #include "ModularHash.h"
 using namespace std;
 
 ifstream inFile;
+
+unsigned int hash_vec(vector<unsigned int> const& vec) {
+  unsigned int seed = vec.size();
+  for(auto& i : vec) {
+    seed ^= i + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+  }
+  return seed;
+}
+
+pair<unsigned int,unsigned int> parella_inc(unsigned int a, unsigned int b){
+    return (a < b) ? make_pair(a,b) : make_pair(b,a);
+}
 
 void fill(vector<vector<unsigned int>> & repMatrix,const set<string> & shingles, const vector<set<string>> & docShing){
 
@@ -73,10 +86,27 @@ int main(int argc, char** argv){
             }
         }
     }
-    printmat(repMatrix);
-    for(int k = 0; k < h; ++k){
-        cout << minHashMod[k].first << " , " << minHashMod[k].second << ": " << prime << endl;
+    set<pair<unsigned int,unsigned int>> candidats;                     //creem el LSH
+    map<unsigned int,vector<unsigned int>> bucket;
+    for(int i = 0; i < h; i+=r){
+        bucket.clear();
+        for(int j = 0; j < argc-1; ++j){
+            vector<unsigned int> row;
+            for(int k = 0; k < r; ++k){
+                row.push_back(signatureMatrix[i+k][j]);
+            }
+            unsigned int doc1 = hash_vec(row);
+            auto it = bucket.find(doc1);            
+            if(it != bucket.end()){
+                for(int l = 0; l < (it->second).size(); ++l ){  //al ser set, ordenem la parella de documents de forma creixent per a evitar repeticions
+                    candidats.insert(parella_inc((it->second)[l], j));
+                    
+                }
+                (it->second).push_back(j);
+            }else{
+                bucket.insert(it, pair<unsigned int, vector<unsigned int> >(doc1, vector<unsigned int>( 1 , j)));                
+            }
+
+        }
     }
-    printmat(signatureMatrix);
-    cout << sim(signatureMatrix, 0, 1) << endl;
 }
