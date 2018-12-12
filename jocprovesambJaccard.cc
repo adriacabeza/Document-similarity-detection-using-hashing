d050f7cf57ba1863e8305554a9e257fe73424770
Carlos Bergillos <carlos98.air.cn@gmail.com>
2018-12-13 00:31:15 +0100

    Csv generator and tests

--- jocprovesambJaccard.cc
+++ jocprovesambJaccard.cc
@@ -2,34 +2,46 @@
 #include "jaccard.h"
 #include <vector>
 #include <string>
+#include <iomanip>
 
 using namespace std;
 ifstream inFile;
-ofstream outFile;
+ofstream outFileSim, outFileTime;
 
 int main(){
-    vector<string> path {"./input_texts/Elretornodelrey.txt","./input_texts/harrypotter1.txt","./input_texts/Starwars.txt"};
-    vector<int> k {3,4,5,6,7,8, 9,10,18,22}; 
-    outFile.open("output.txt");
-    for(int i = 0; i < k.size(); ++i){
-        for(int j = 0; j < 3; ++j){
-            clock_t start = clock();
-            inFile.open(path[j]);
-            set<string> A = kshingles(&inFile ,k[i],true,false,false);
-            inFile.close();
+    set<string> A, B;
+    //vector<string> paths {"./input_texts/Elretornodelrey.txt","./input_texts/harrypotter1.txt","./input_texts/Starwars.txt", "./input_texts/lorem.txt"};
+    vector<string> paths {"./input_texts/Elretornodelrey.txt","./input_texts/harrypotter1.txt","./input_texts/Starwars.txt"};
+    vector<int> ks {3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+    //vector<int> ks {15, 16};
+    string filename = "out";
+    outFileSim.open(filename+"_sim.txt");
+    outFileSim << setprecision(10) << fixed;
+    outFileTime.open(filename+"_time.txt");
+    outFileTime << setprecision(6) << fixed;
+    for(int k : ks){
+        outFileSim << k;
+        outFileTime << k;
+        for(int i = 0; i < paths.size(); ++i){
+            for(int j = i+1; j < paths.size(); ++j){
+                clock_t start = clock();
+                inFile.open(paths[i]);
+                A = kshingles(&inFile,k,true,false,false);
+                inFile.close();
 
-            inFile.open(path[(j+1)%3]); 
-            set<string> B = kshingles(&inFile,k[i],true,false,false);
-            inFile.close();
-            // std::ios_base::app
-            
-            
-            outFile<<"Jaccard Similarity between "<< path[j]<< " and "<< path[(j+1)%3]<< " using " << k[i] << "-shingles is "<< Jaccard(A,B)<<endl;
-            cout << "Jaccard Similarity between "<< path[j]<< " and "<< path[(j+1)%3]<< " using " << k[i] << "-shingles is "<< Jaccard(A,B)<<endl;
-            outFile<<"Time "<<double(clock()-start)/CLOCKS_PER_SEC <<endl<<endl;
-         
+                inFile.open(paths[j]);
+                B = kshingles(&inFile,k,true,false,false);
+                inFile.close();
+
+                outFileSim << ",\t" << Jaccard(A,B);
+                outFileTime << ",\t" << double(clock()-start)/CLOCKS_PER_SEC;
+                cout << "Done: K = " << k << "\t" << paths[i] << "\t- " << paths[j] << endl;
+            }
         }
+        outFileSim << endl;
+        outFileTime << endl;
     }
-    outFile.close();
+    outFileSim.close();
+    outFileTime.close();
     return 0;
 }
