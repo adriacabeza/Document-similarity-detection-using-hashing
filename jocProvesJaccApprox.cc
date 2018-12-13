6f539dbff831738ea50abf379d13bc36d4f527ab
Carlos Bergillos <carlos98.air.cn@gmail.com>
2018-12-13 08:23:56 +0100

    Hash functions times csv

--- jocProvesJaccApprox.cc
+++ jocProvesJaccApprox.cc
@@ -11,8 +11,6 @@ ofstream outFileSim, outFileTime;
 
 int main(){
     set<string> A, B;
-    //vector<string> paths {"./input_texts/Elretornodelrey.txt","./input_texts/harrypotter1.txt","./input_texts/Starwars.txt", "./input_texts/lorem.txt"};
-    //vector<string> paths {"./input_texts/Elretornodelrey.txt","./input_texts/harrypotter1.txt","./input_texts/Starwars.txt"};
     cout << "Enter filename: (without .txt)" << endl;
     string filename;
     cin >> filename;
@@ -27,22 +25,19 @@ int main(){
         inFile.open(input+to_string(i)+".txt");
     }
     inFile.close();
-    /**vector<int> ks {3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+    vector<int> ks {3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
     //vector<int> ks {15, 16};
-    outFileSim.open("./output_data/"+filename+"_out_sim.txt");
-    outFileSim << setprecision(10) << fixed;
-    outFileTime.open("./output_data/"+filename+"_out_time.txt");
-    outFileTime << setprecision(6) << fixed;**/
+    outFileTime.open("./output_data/"+filename+"_hashfuncts_time.txt");
+    outFileTime << setprecision(6) << fixed;
 
-    //
-    int k = 9;
+    //int k = 9;
     int b = 5;
     int r = 5;
     int h = b*r;
-    //
-    //for(int k : ks){
-        //outFileSim << k;
-        //outFileTime << k;
+
+    for(int k : ks){
+
+        outFileTime << k;
         set<string> shingles_union;
         vector<set<string>> shingles_doc(paths.size());
         for(int p = 0; p < paths.size(); ++p){
@@ -53,33 +48,36 @@ int main(){
         }
 
         vector<vector<unsigned int> > charactMatrix =  characteristicMatrix(shingles_doc, shingles_union);
-        vector<vector<unsigned int> > signatureMatrix =  murmurHashing(charactMatrix, h);
-        set<pair<unsigned int, unsigned int> > candidates = LSH(signatureMatrix, r, h);
-        printmat(signatureMatrix);
+        vector<vector<unsigned int> > signatureMatrix;
+
+        clock_t start = clock();
+
+        //Modular Hashing
+        start = clock();
+        signatureMatrix =  modularHashing(charactMatrix, h);
+        outFileTime << "\t" << double(clock()-start)/CLOCKS_PER_SEC;
+
+        //Multiplicative Hashing
+        start = clock();
+        signatureMatrix =  modularHashing(charactMatrix, h);
+        outFileTime << "\t" << double(clock()-start)/CLOCKS_PER_SEC;
+
+        //Murmur Hashing
+        start = clock();
+        signatureMatrix =  modularHashing(charactMatrix, h);
+        outFileTime << "\t" << double(clock()-start)/CLOCKS_PER_SEC;
+
+        /**set<pair<unsigned int, unsigned int> > candidates = LSH(signatureMatrix, r, h);
         for(pair<unsigned int, unsigned int> p : candidates){
             int a = p.first;
             int b = p.second;
             float sim_v = sim(signatureMatrix, a, b);
-            //cout << "Candidate " << paths[a] << " with " << paths[b] << " has similarity " << sim_v << endl;
-        }
-        ///////
-                /**clock_t start = clock();
-                inFile.open(paths[i]);
-                A = kshingles(&inFile,k,true,false,false);
-                inFile.close();
+            cout << "Candidate " << paths[a] << " with " << paths[b] << " has similarity " << sim_v << endl;
+        }**/
 
-                inFile.open(paths[j]);
-                B = kshingles(&inFile,k,true,false,false);
-                inFile.close();
-
-                outFileSim << "\t" << Jaccard(A,B);
-                outFileTime << "\t" << double(clock()-start)/CLOCKS_PER_SEC;
-                cout << "Done: K = " << k << "\t" << paths[i] << "\t- " << paths[j] << endl;**/
-        //}
-        //outFileSim << endl;
-        //outFileTime << endl;
-    //}
-    //outFileSim.close();
-    //outFileTime.close();
+        cout << "Done: K = " << k << endl;
+        outFileTime << endl;
+    }
+    outFileTime.close();
     return 0;
 }
